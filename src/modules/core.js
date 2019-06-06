0863fede184de0f1fe6a56ba8f21cc76d7c5bd2a
khem <khem@mycostech.com>
2019-06-06 10:37:48 +0700

    Add Single Comparison mode

--- src/modules/core.js
+++ src/modules/core.js
@@ -1,32 +1,39 @@
 var fs = require('fs')
 var shell = require('shelljs')
 
+function compareDirectory(sourcePath, targetPath) {
+  const originalFiles = new Set(fs.readdirSync(sourcePath))
+  const delList = []
+  fs.readdirSync(targetPath).forEach(file => {
+    if (originalFiles.has(file))
+      originalFiles.delete(file)
+    else 
+      delList.push(file)
+  })
+  const addList = Array.from(originalFiles)
+
+  return {
+    'sourcePath': sourcePath,
+    'targetPath': targetPath,
+    'add': addList,
+    'delete': delList
+  }
+}
+
 export default {
   processScript: function(scriptPath){
     const rawContent = fs.readFileSync(scriptPath).toString()
     const lines = rawContent.split('\r\n').filter(r => r.length > 0)
     const results = []
     for (let i = 0; i < lines.length; i += 2) {
-      const originalFiles = new Set(fs.readdirSync(lines[i]))
-      const delList = []
-      fs.readdirSync(lines[i + 1]).forEach(file => {
-        if (originalFiles.has(file))
-          originalFiles.delete(file)
-        else 
-          delList.push(file)
-      })
-      const addList = Array.from(originalFiles)
-
-      results.push({
-        'id': (i / 2) + 1,
-        'sourcePath': lines[i],
-        'targetPath': lines[i + 1],
-        'add': addList,
-        'delete': delList
-      })
+      var result = compareDirectory(lines[i], lines[i + 1])
+      results.push(result)
     }
 
     return results
+  },
+  compare: function(source, target) {
+    return compareDirectory(source, target)
   },
   sync: function(source, target) {
     return new Promise(function(resolve, reject){
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,56 @@
   <v-container>
     <v-layout wrap>
       <v-flex xs12>
-        <v-btn color="info" @click="selectFile()" class="ma-0">Browse for Script File...</v-btn>
-        <v-btn 
-          color="success" 
-          :disabled="previewResults.length == 0 || previewResults.every(e => e.hasFinished)" 
-          @click="syncAll()">
-          Sync All
-        </v-btn>
+        <v-tabs 
+          v-model="activeTab"
+          color="cyan" 
+          dark 
+          slider-color="yellow"
+        >
+          <v-tab>Single Folder</v-tab>
+          <v-tab>Multiple Folders</v-tab>
+          <!-- Single Dir -->
+          <v-tab-item>
+            <v-layout wrap>
+              <v-flex xs6 class="pt-2">
+                <v-text-field
+                  v-model="selectedDir.sourceDir"
+                  label="Source Directory"
+                  box
+                  hide-details
+                  readonly
+                ></v-text-field>
+                <v-btn class="ml-0" color="info" @click="selectDir('source')">Browse...</v-btn>
+              </v-flex>
+              <v-flex xs6 class="pt-2 pl-2">
+                <v-text-field
+                  v-model="selectedDir.targetDir"
+                  label="Target Directory"
+                  box
+                  hide-details
+                  readonly
+                ></v-text-field>
+                <v-btn class="ml-0" color="info" @click="selectDir('target')">Browse...</v-btn>
+              </v-flex>
+              <v-flex xs12>
+                <v-btn color="success" class="ml-0" :disabled="selectedDir.sourceDir === '' || selectedDir.targetDir === ''" @click="compareDir()">Compare</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-tab-item>
+          <!-- Multiple Dir -->
+          <v-tab-item>
+            <div>
+              <v-btn color="info" @click="selectFile()" class="ma-0">Browse for Script File...</v-btn>
+              <v-btn 
+                color="success" 
+                :disabled="previewResults.length == 0 || previewResults.every(e => e.hasFinished)" 
+                @click="syncAll()">
+                Sync All
+              </v-btn>
+            </div>
+          </v-tab-item>
+        </v-tabs>
+        
       </v-flex>
       <v-flex xs12 v-for="r in previewResults" v-bind:key="r.id" class="mt-2">
         <v-card v-bind:class="{ syncing: r.syncing, finished: r.hasFinished }">
@@ -62,11 +105,40 @@
 <script>
   const { dialog } = require('electron').remote
   import core from '../modules/core'
+  
+  function newCompareResult(result, index) {
+    return {
+      id: index,
+      data: result,
+      hasShowDetail: false,
+      syncing: false,
+      hasFinished: result.add.length === 0 && result.delete.length === 0,
+      showDetail: function() {
+        this.hasShowDetail = !this.hasShowDetail
+      },
+      sync: function() {            
+        this.syncing = true
+        core.sync(this.data.sourcePath, this.data.targetPath).then(() => {
+          this.hasFinished = true
+        })
+        .catch(err => {
+          alert(err)
+        }).finally(() => {
+          this.syncing = false
+        })
+      }
+    }
+  }
 
   export default {
     data() {
       return {
-        previewResults: []
+        previewResults: [],
+        selectedDir: {
+          sourceDir: '',
+          targetDir: ''
+        },
+        activeTab: 0
       }
     },
     components: {
@@ -79,35 +151,31 @@
         })
         if (filePath && filePath.length > 0) {
           try {
-            this.previewResults = core.processScript(filePath[0]).map(r => ({
-              data: r,
-              hasShowDetail: false,
-              syncing: false,
-              hasFinished: r.add.length === 0 && r.delete.length === 0,
-              showDetail: function() {
-                this.hasShowDetail = !this.hasShowDetail
-              },
-              sync: function() {            
-                this.syncing = true
-                core.sync(this.data.sourcePath, this.data.targetPath).then(() => {
-                  this.hasFinished = true
-                })
-                .catch(err => {
-                  alert(err)
-                }).finally(() => {
-                  this.syncing = false
-                })
-              }
-            }))
+            this.previewResults = core.processScript(filePath[0]).map((r, idx) => newCompareResult(r, idx))
           } catch (err) {
             alert(err)
           }
         }
       },
+      compareDir: function() {
+        this.previewResults = [newCompareResult(core.compare(this.selectedDir.sourceDir, this.selectedDir.targetDir), 0)]
+      },
       syncAll: function() {
         this.previewResults.forEach(vm => {
           vm.sync()
         })
+      },
+      selectDir: function(mode) {
+        const path = dialog.showOpenDialog({
+          title: 'Select Directory',
+          properties: ['openDirectory']
+        })
+        if (path && path.length > 0) {
+          if (mode === 'source')
+            this.selectedDir.sourceDir = path[0]
+          else
+            this.selectedDir.targetDir = path[0]
+        }
       }
     }
   }
